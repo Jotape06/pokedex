d66312091f7bc2afc7bd37be50d20f5ae6347aae
Jotape06 <joaoflwvlw@gmail.com>
2022-12-17 23:36:34 -0300

    test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-        <h1 className='footerTitle'>Desenvolvido por João Paulo</h1>
+        <h1 className='footerTitle'>Desenvolvido por João Paulo.</h1>
     </footer>
   )
 }
